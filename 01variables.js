ced0cfe52d021d1bf03c40f54a78426f27611695
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-05 21:06:54 +0600

    var-let-constAndComments

---
+++ 01variables.js
@@ -0,0 +1,16 @@
+const accountId = 122560;
+let accountEmail = "jahid@gmail.com";
+var accountPass = "1234";
+accountCity = "Panchagarh";
+let accountState;
+
+// accountId = 2; //mot allowed
+
+/*
+We don`t use var 
+Because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountEmail, accountPass, accountCity, accountState]);
