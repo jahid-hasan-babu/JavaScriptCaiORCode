e766951b98fb365982212be75d4a551be961c05c
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-08 00:05:50 +0600

    operator

--- 03ConversesionOperation.js
+++ 03ConversesionOperation.js
@@ -1,12 +1,12 @@
 // let score = "55abc";
 let score = null;
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber); //Number
-console.log(valueInNumber); //NaN
+// console.log(typeof valueInNumber); //Number
+// console.log(valueInNumber); //NaN
 
 // "55" => 33
 // "55abb" => NaN type number
@@ -14,7 +14,7 @@ console.log(valueInNumber); //NaN
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1 => true
 // 0 => false
 // "" => false
@@ -22,5 +22,36 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// .................. Operations .......................
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 3);
+// console.log(2 % 1);
+
+let str1 = "Jahid";
+let str2 = "Hasan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log(true);
+// console.log(+true); //cause boolean
+// console.log(true+)
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 1;
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
