9587c252d79107081223ebd86b4bdbc76d462c28
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-19 20:55:02 +0600

    objectand api

---
+++ 14objectandapi.js
@@ -0,0 +1,17 @@
+const course = {
+  courseName: "JS in Bangla",
+  price: "1555",
+};
+
+//course.courseName
+
+const { price: p } = course;
+
+// console.log(price);
+console.log(p);
+
+// {
+//   "name": "Jahid",
+//   "price": "4452",
+
+// }
